301d96027b39edd5487e6f4ae68fe40501b3bf53
azaraxx <azaraxx@hotmail.com>
2022-12-05 22:05:30 +0300

    Tailwind basics with Vue

---
+++ tailwind-vite/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="container px-6">
+    <p class="bg-green-700 text-gray-100">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna
+      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+      sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <p class="bg-amber-400 text-gray-700">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna
+      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+      sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <pre>
+    console.log
+    </pre>
+    <div class="mt-[25px] bg-red-100 sm:bg-blue-200 md:bg-yellow-300 lg:bg-green-400 xl:bg-purple-500">
+      <button class="bg-blue-500 text-white px-3 py-2 mx-5 my-5 rounded-md">Button</button>
+      <button class="button">Button</button>
+      <button class="button2">Button</button>
+    </div>
+  </div>
+</template>
---
+++ tailwind-vite/tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  darkMode: true,
+  theme: {
+    extend: {
+      colors: {
+        // amber: colors.amber
+      }
+    },
+  },
+  plugins: [],
+}
